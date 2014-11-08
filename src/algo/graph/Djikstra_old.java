65ecc8053095a4d79c39e860841c459ee61af4d0
Pascal <pascalnnlacsap@gmail.com>
2014-11-08 19:28:27 +0100

    Clean

--- src/algo/graph/Djikstra_old.java
+++ src/algo/graph/Djikstra_old.java
@@ -6,12 +6,12 @@ import java.util.List;
 
 import algo.graph.Node;
 
-public class Djikstra<T> {
+public class Djikstra_old<T> {
 
 	private Graph graph;
 	private LinkedPriorityQueue listePassage;
 
-	public Djikstra(Graph g) {
+	public Djikstra_old(Graph g) {
 		this.graph = g;
 		this.listePassage = new LinkedPriorityQueue();
 	}
@@ -21,38 +21,57 @@ public class Djikstra<T> {
 		if (nodeDepart == null || nodeArrive == null) {
 			return;
 		}
-
-		List<Node> listNoeud = new ArrayList<Node>();
+		// Liste qui contiendra nos List<Nodes> qui represente un parcours
 		LinkedPriorityQueue newListePassage = new LinkedPriorityQueue();
 
+		// Node
 		Node nodePere = nodeDepart;
 		Node nodeFils = null;
+
+		// liste de noeud qui sera dans LinkPriorityQueue
+		List<Node> listNoeud = new ArrayList<Node>();
 		List<Node> listNoeudActuel = null;
 
+		// on ajoute le point de départ qui est pour l'instant notre seule
+		// parcours
 		listNoeud.add(nodePere);
+		// on l'ajoute dans notre liste de parcours
 		listePassage.add(listNoeud);
+
+		// on check si on a des parcours
 		for (int i = 0; i < listePassage.size(); i++) {
 
 			listNoeudActuel = listePassage.peek();
+			System.out.println(" Top : " + listNoeudActuel.size());
 			nodePere = listNoeudActuel.get(listNoeudActuel.size() - 1);
 			System.out.println("NodePere = " + nodePere.getId());
 
 			for (Edge e : nodePere.getEdges()) {
-
 				nodeFils = e.getOther(nodePere);
-				System.out.println(nodeFils.getId() + ":" + e.getDistance());
+				System.out.println(nodePere.getId() + " --> "
+						+ nodeFils.getId());
 
 				int actualSize = listNoeudActuel.size();
-				System.out.println(" taille = "+actualSize);
-				for (int k = 0; k < actualSize; k++) {
-					listNoeud.add(listNoeudActuel.get(k));
-				}
+				listNoeud = new ArrayList<Node>();
+				listNoeud.add(nodePere);
+				// for (int k = 0; k < actualSize; k++) {
+				// listNoeud.add(listNoeudActuel.get(k));
+				// }
 				listNoeud.add(nodeFils);
+				for (int z = 0; z < listNoeud.size(); z++) {
+					System.out.print("-->" + listNoeud.get(z).getId());
+				}
+				listNoeud.remove(nodePere);
 
+				System.out.println("");
 				newListePassage.add(listNoeud);
+				System.out.println(" Taille : " + newListePassage.size());
 
 			}
+
 		}
+
+		// a remonter d'un cran
 		listePassage = newListePassage;
 	}
 }
